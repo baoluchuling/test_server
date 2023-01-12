9112a4a4addfba503db9f9a56906777ff2ab2f38
baoluchuling <baoluchuling@126.com>
2023-01-12 16:17:55 +0800

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -4,9 +4,27 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  
+  
+  const urlPath = req.url;
+  if (urlPath === "/") {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  } else if (urlPath === "/overview") {
+    res.statusCode = 200;
+    res.end('Welcome to the "overview page" of the nginX project');
+  } else if (urlPath === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        product_id: "xyz12u3",
+        product_name: "NginX injector",
+      })
+    );
+  } else {
+    res.end("Successfully started a server");
+  }
 });
 
 server.listen(port, hostname, () => {
