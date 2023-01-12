7e16524c5034a982a7eb0f142a634a479a5480ba
baoluchuling <baoluchuling@126.com>
2023-01-12 16:21:02 +0800

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say Hello to Mary", r.Host)
+	} else {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Printf("%s", err)
+	}
+}
